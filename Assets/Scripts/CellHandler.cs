35e44c9de704f21e707dc0b629949323982f5320
Vladislav Igorevich <vladislav.igorevich.workmail@gmail.com>
2021-12-01 00:50:14 +0300

    clear comments

--- Assets/Scripts/CellHandler.cs
+++ Assets/Scripts/CellHandler.cs
@@ -16,70 +16,15 @@ public class CellHandler : MonoBehaviour, IPointerClickHandler
     public ClickEvent OnClick;
     public void OnPointerClick(PointerEventData eventData) => OnClick.Invoke(this);
 
-    //private Vector3 _scale;
-    //private Vector3 _cellScale;
+
     private CardData _cardData;
     private SpriteRenderer _elementSpriteRenderer;
-
-    void Start()
-    {
-        //_scale = _element.transform.localScale;
-        //_cellScale = transform.localScale;
-    }
-
-    void Update()
-    {
-        
-    }
 
     public void SetCardData(CardData cardData)
     {
         _cardData = cardData;
         _element.GetComponent<SpriteRenderer>().sprite = cardData.Sprite;
-
-        //_elementSpriteRenderer.sprite = cardData.Sprite;
     }
-
-    //public void ShakeElement()
-    //{
-    //    Transform elementTransform = _element.transform;
-    //    var seq = DOTween.Sequence();
-    //    seq.Append(elementTransform.DOScale(_scale * 0.98f, 0.04f));
-    //    seq.Append(elementTransform.DOScale(_scale * 0.99f, 0.04f));
-    //    seq.Append(elementTransform.DOScale(_scale * 0.94f, 0.10f));
-    //    seq.Append(elementTransform.DOScale(_scale * 0.98f, 0.08f));
-    //    seq.Append(elementTransform.DOScale(_scale * 0.75f, 0.2f));
-    //    seq.Append(elementTransform.DOScale(_scale * 0.98f, 0.18f));
-    //    seq.Append(elementTransform.DOScale(_scale * 0.44f, 0.12f));
-    //    seq.Append(elementTransform.DOScale(_scale * 1f, 0.24f));
-    //}
-
-    //public void BounceElement(bool bounceCell = false)
-    //{
-    //    var seq = DOTween.Sequence();
-    //    Vector3 scale;
-    //    Transform elementTransform;
-
-    //    if (bounceCell)
-    //    {
-    //        //transform.localScale = Vector3.zero;
-    //        elementTransform = transform;
-    //        scale = _cellScale;
-    //        //EnableSprites();
-    //    }
-    //    else
-    //    {
-    //        elementTransform = _element.transform;
-    //        scale = _scale;
-    //    }
-
-
-    //    seq.Append(elementTransform.transform.DOScale(scale * 1.2f, 0.2f));
-    //    seq.Append(elementTransform.transform.DOScale(scale * 0.95f, 0.2f));
-    //    seq.Append(elementTransform.transform.DOScale(scale * 1.1f, 0.2f));
-    //    seq.Append(elementTransform.transform.DOScale(scale * 0.98f, 0.2f));
-    //    seq.Append(elementTransform.transform.DOScale(scale * 1f, 0.2f));
-    //}
 
     public void EnableSprites()
     {
